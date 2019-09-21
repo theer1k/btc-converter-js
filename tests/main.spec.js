6eae734887bb56465c1de71092d4ca13434a69a2
Erik Cunha <erik.cunha@venturus.org.br>
2019-09-21 00:45:53 -0300

    Add first test of CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,18 @@
 import { expect } from 'chai';
 import { it, describe } from 'mocha';
 
-describe('Main', () => {
-  it('should do what..', () => expect(true).to.be.true);
+import { exec } from 'child_process';
+
+const btcConverter = './src/main.js';
+
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      if (err) throw err;
+
+      expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+
+      done();
+    });
+  });
 });
